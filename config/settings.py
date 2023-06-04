f5875395e2ec6aea4ca6c9ce68812c9d804cf9e0
rustam41k0 <rustamskysan@gmail.com>
2023-06-04 20:50:54 +0500

    refactoring

--- config/settings.py
+++ config/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),  # 'localhost',
+        'HOST': os.getenv('DB_HOST'),
         'PORT': 5432,
     }
 }
